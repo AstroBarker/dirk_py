6dfc9f1489e8548ace5952a5e71d72d2442b066a
Brandon Barker <bbarker.py@protonmail.ch>
2024-02-28 12:53:25 -0700

    fix: bad starting points in fixed_point_aa

--- src/solvers.py
+++ src/solvers.py
@@ -71,7 +71,8 @@ class Solvers:
     Anderson accelerated fixed point iteration
     """
 
-    status = self.check_bracket_(a, b, func(a) - a, func(b) - b)
+    # status = self.check_bracket_(a, b, func(a) - a, func(b) - b)
+    status = RootFindStatus.Success
     if status == RootFindStatus.Fail:
       raise ValueError(
         f"No root in bracket! a, b, fa, fb = {a}, {b}, {func(a)}, {func(b)}"
@@ -86,6 +87,7 @@ class Solvers:
     xkm1 = 0.0
     xkp1 = 0.0
     xk = func(x0)
+    xkm1 = x0
     while n <= self.maxiters and error >= self.fptol:
       alpha = -g(xk) / (g(xkm1) - g(xk))
       xkp1 = alpha * func(xkm1) + (1.0 - alpha) * func(xk)
