6fd754187d362956e619d16ab55ba03e93d56d54
Brandon Barker <bbarker.py@protonmail.ch>
2024-02-29 21:11:26 -0700

    feat: add newton iteration, and anderson accelerated version

--- src/solvers.py
+++ src/solvers.py
@@ -60,7 +60,6 @@ class Solvers:
 
       if n == self.maxiters:
         print(" ! Not converged!")
-
       ans = x1
     return ans
 
@@ -100,4 +99,68 @@ class Solvers:
         print(" ! Not converged!")
     return xk
 
-  # End fixed_point
+  # End fixed_point_aa
+
+  def newton(self, func, dfunc, x0, a, b):
+    """
+    Newton-Raphson iteration
+    """
+    status = self.check_bracket_(a, b, func(a) - a, func(b) - b)
+    if status == RootFindStatus.Fail:
+      raise ValueError(
+        f"No root in bracket! a, b, fa, fb = {a}, {b}, {func(a)}, {func(b)}"
+      )
+    # TODO: implement bisection and drop into bisection if fail
+
+    n = 0
+    h = func(x0) / dfunc(x0)
+    error = 1.0
+    while n <= self.maxiters and error >= self.fptol:
+      xn = x0
+      h = func(xn) / dfunc(xn)
+      x0 = xn - h
+      error = abs(xn - x0)
+      n += 1
+
+      if n == self.maxiters:
+        print(" ! Not converged!")
+    return x0
+
+  # End newton
+
+  def newton_aa(self, func, dfunc, x0, a, b):
+    """
+    Anderson accelerated Newton-Raphson iteration
+    """
+    status = self.check_bracket_(a, b, func(a) - a, func(b) - b)
+    if status == RootFindStatus.Fail:
+      raise ValueError(
+        f"No root in bracket! a, b, fa, fb = {a}, {b}, {func(a)}, {func(b)}"
+      )
+    # TODO: implement bisection and drop into bisection if fail
+
+    n = 0
+    h = func(x0) / dfunc(x0)
+    error = 1.0
+    xk = x0 - h
+    xkm1 = x0
+    xkp1 = 0.0
+    while n <= self.maxiters and error >= self.fptol:
+      hp1 = func(xk) / dfunc(xk)
+      h = func(xkm1) / dfunc(xkm1)
+
+      # Anderson acceleration step
+      gamma = hp1 / (hp1 - h)
+
+      xkp1 = xk - hp1 - gamma * (xk - xkm1 - hp1 + h)
+      error = abs(xk - xkp1)
+
+      xkm1 = xk
+      xk = xkp1
+
+      n += 1
+      if n == self.maxiters:
+        print(" ! Not converged!")
+    return xk
+
+  # End newton_aa
--- src/test.py
+++ src/test.py
@@ -12,6 +12,14 @@ def func_fp(x):
   return np.cos(x)
 
 
+def func_newton(x):
+  return np.cos(x) - x
+
+
+def dfunc_newton(x):
+  return -np.sin(x) - 1.0
+
+
 def test_fp():
   """
   Test fixed point iteration
@@ -60,6 +68,48 @@ def test_fp_aa():
 # End test_fp_aa
 
 
+def test_newton():
+  """
+  Test newton iteration
+  """
+
+  max_iters = 100
+  fptol = 1.0e-8
+
+  solver = Solvers(max_iters, fptol)
+
+  x0 = 1.0
+  a = 0.0
+  b = 1.0
+
+  my_ans = solver.newton(func_newton, dfunc_newton, x0, a, b)
+
+  ANS = 0.739085133215160641
+
+  assert abs(my_ans - ANS) < fptol, f"Root must be within {fptol}"
+
+
+def test_newton_aa():
+  """
+  Test newton iteration
+  """
+
+  max_iters = 100
+  fptol = 1.0e-8
+
+  solver = Solvers(max_iters, fptol)
+
+  x0 = 1.0
+  a = 0.0
+  b = 1.0
+
+  my_ans = solver.newton_aa(func_newton, dfunc_newton, x0, a, b)
+
+  ANS = 0.739085133215160641
+
+  assert abs(my_ans - ANS) < fptol, f"Root must be within {fptol}"
+
+
 def test_dirk22():
   dirk = DIRK(2, 2, 0.1)
   dirk.evolve(func1, 1.0)
@@ -74,3 +124,6 @@ if __name__ == "__main__":
   # main
   test_fp()
   test_fp_aa()
+  test_dirk22()
+  test_newton()
+  test_newton_aa()
